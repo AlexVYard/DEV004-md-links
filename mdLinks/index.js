dedfaba1d80e7f8eaad01857117103c6eb8a50ff
AlexVYard <alexvegayard@gmail.com>
2023-03-20 14:15:00 -0500

    se pueden ejecutar comandos usando "node mdLinks"

---
+++ mdLinks/index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+/**
+ * md-links-alexv
+ * check links
+ *
+ * @author Alexandra F. Vega <https://github.com/AlexVYard/>
+ */
+
+// const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(() => {
+	// init({ clear });
+	input.includes(`help`) && cli.showHelp(0);
+
+	debug && log(flags);
+
+	if (input.includes('validate')) {
+		const validating = new Promise((resolve, reject) => {
+			resolve(flags.validate = true);
+			reject('Not a Success!')
+		});
+		validating.then(() => {
+			console.log("flags.validate = "+flags.validate)
+		})
+	}
+
+ 	if (input.includes('stats')) {
+		const stating = new Promise((resolve, reject) => {
+			resolve(flags.stats = true);
+			reject('Not a Success!')
+		});
+		stating.then(() => {
+			console.log("flags.stats = "+flags.stats)
+		})		
+	}
+
+})();
---
+++ mdLinks/utils/cli.js
@@ -0,0 +1,56 @@
+const meow = require('meow');
+const meowHelp = require('cli-meow-help');
+
+const flags = {
+	clear: {
+		type: `boolean`,
+		default: false,
+		alias: `c`,
+		desc: `Clear the console`
+	},
+	noClear: {
+		type: `boolean`,
+		default: false,
+		desc: `Don't clear the console`
+	},
+	debug: {
+		type: `boolean`,
+		default: false,
+		alias: `d`,
+		desc: `Print debug info`
+	},
+	version: {
+		type: `boolean`,
+		desc: `Print CLI version`
+	},
+	validate: {
+		type: `boolean`,
+		default: false,
+		alias: `v`,
+		desc: `Validate links`
+	},
+	stats: {
+		type: `boolean`,
+		alias: `s`,
+		desc: `Show stats`
+	}
+};
+
+const commands = {
+	help: { desc: `Print help info` }
+};
+
+const helpText = meowHelp({
+	name: `md-links-alexv`,
+	flags,
+	commands
+});
+
+const options = {
+	inferType: true,
+	description: false,
+	hardRejection: false,
+	flags
+};
+
+module.exports = meow(helpText, options);

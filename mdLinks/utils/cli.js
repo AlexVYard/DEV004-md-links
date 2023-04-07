496ed5ee1aa240c00368cdee4c10cb961c4f3a16
AlexVYard <alexvegayard@gmail.com>
2023-04-07 09:44:46 -0500

    codigo optimizado

--- mdLinks/utils/cli.js
+++ mdLinks/utils/cli.js
@@ -1,8 +1,8 @@
-const meow = require('meow');
-const meowHelp = require('cli-meow-help');
+/* const meow = require('meow');
+const meowHelp = require('cli-meow-help'); */
 
 const program = require('commander');
-const options = program.opts();
+// const options = program.opts();
 
 // import LinkChecker from 'linkinator';
 
@@ -11,7 +11,6 @@ program
 	.option('-v, --validate', 'Validate links, use with a route')
 	.option('-s, --stats', 'Show stats, use with a route')
 	.action((route1, options) => {
-
 
 		const { readFileSync } = require('fs');
 		const markdownLinkExtractor = require('markdown-link-extractor');
@@ -22,8 +21,22 @@ program
 		const validate = options.validate ? 1 : 0;
 		const stats = options.stats ? 1 : 0;
 
-		if (options.validate) {
-			// myPromise.then(() => {
+		let brokenLinks = 0
+
+		const brokenPromise = new Promise((resolve) => {
+				import('linkinator')
+					.then(module => {
+						const checker = new module.LinkChecker()
+						checker.on("link", (link) => {
+							if (link.state === "BROKEN") {
+								brokenLinks++
+							}
+						})
+						resolve(checker.check({ path: route1 }))
+					});
+		})
+
+		if (validate === 1) {
 			const markdownLinkCheck = require('markdown-link-check');
 			links.forEach(link => {
 				markdownLinkCheck(link, (err, results) => {
@@ -38,34 +51,9 @@ program
 					})
 				})
 			})
-			// })
 		}
 
-		let brokenLinks = 0
-
-		const brokenPromise = new Promise((resolve) => {
-			import('linkinator')
-				.then(module => {
-					// console.log("module es: ", module)
-					// console.log(module.LinkChecker)
-					// console.log("PROCESANDO, ESTA LENTO")
-
-					const checker = new module.LinkChecker()
-
-					checker.on("link", (link) => {
-						if (link.state === "BROKEN") {
-							brokenLinks++
-							// console.log(link["url"])
-						}
-					})
-
-					// const myPromise = new Promise((resolve) => {
-					resolve(checker.check({ path: route1 }))
-					// });
-				});
-		})
-
-		if (options.stats) {
+		if (stats === 1) {
 			let duplicatesTotal = links.filter((item, index) => links.indexOf(item) !== index).length
 			console.log("Total: " + links.length)
 			console.log("Unique: " + (links.length - duplicatesTotal))
@@ -77,196 +65,6 @@ program
 		}
 
 	})
-		// 'use strict';
-
-		/* 		const link = require('linkinator');
-		
-				async function complex() {
-					// create a new `LinkChecker` that we'll use to run the scan.
-					const checker = new link.LinkChecker();
-		
-					// Respond to the beginning of a new page being scanned
-					checker.on('pagestart', url => {
-						console.log(`Scanning ${url}`);
-					});
-		
-					// After a page is scanned, check out the results!
-					checker.on('link', result => {
-		
-						// check the specific url that was scanned
-						console.log(`  ${result.url}`);
-		
-						// How did the scan go?  Potential states are `BROKEN`, `OK`, and `SKIPPED`
-						console.log(`  ${result.state}`);
-		
-						// What was the status code of the response?
-						console.log(`  ${result.status}`);
-		
-						// What page linked here?
-						console.log(`  ${result.parent}`);
-					});
-				} */
-
-		// import LinkChecker from 'linkinator';
-		// const { LinkChecker } = require('linkinator');
-
-		/* 		var { HtmlUrlChecker } = require("broken-link-checker");
-				var brokenUrlList = [];
-		
-				const processedLinks = links.filter(link => link.match(/^(https?:\/\/[-a-zA-Z0-9@:%._\+~#=])/));
-		
-				const defaultBaseUrlList = processedLinks
-				async function main() {
-		
-					async function urlChecker(url) {
-		
-						await new Promise(resolve => {
-		
-							const htmlUrlChecker = new HtmlUrlChecker(
-								{
-									excludeInternalLinks: false,
-									excludeExternalLinks: false,
-									excludedKeywords: ['*linkedin*'],
-									filterLevel: 0,
-									acceptedSchemes: ["http", "https"],
-									requestMethod: "get"
-								},
-								{
-									"error": (error) => {
-										console.error(error);
-									},
-									"link": (result) => {
-										try {
-											let brokenLink = `${result.http.response.statusCode} => ${result.url.resolved}`;
-											console.log(brokenLink);
-		
-											if (result.broken) {
-												if (result.http.response && ![undefined, 200].includes(result.http.response.statusCode)) {
-		
-													var urlCrawlResult = new Object();
-													urlCrawlResult.status = result.http.response.statusCode;
-													urlCrawlResult.url = result.url.resolved;
-													urlCrawlResult.htmlBaseUrl = result.base.original;
-													urlCrawlResult.statusMessage = result.http.response.statusMessage;
-		
-													brokenUrlList.push(urlCrawlResult);
-												}
-											}
-										} catch (error) {
-											console.log(error)
-										}
-		
-									},
-									"end": () => {
-										console.log("base url check completed..");
-										resolve();
-									}
-								}
-							);
-		
-							try {
-								htmlUrlChecker.enqueue(url);
-		
-							} catch (error) {
-								console.log(error)
-							}
-		
-						});
-					}
-		
-					async function checkAndGetResults() {
-						console.log(defaultBaseUrlList)
-						for (let baseUrl of defaultBaseUrlList) {
-							await urlChecker(baseUrl);
-						}
-		
-						await new Promise(resolve => {
-							if (brokenUrlList.length > 0) {
-								brokenUrlList.forEach(url => {
-									let message = "\nurl is: " + url.url + "\n"
-										+ "html base url is: " + url.htmlBaseUrl + "\n"
-										+ "status message is: " + url.statusMessage + "\n"
-										+ "status is: " + url.status + "\n";
-		
-									console.log(message);
-								});
-							}
-							else {
-								console.log("\nJob Completed.. There is no broken links!!")
-							}
-		
-							resolve();
-						});
-					}
-		
-					async function executeJob() {
-						await checkAndGetResults();
-						process.exit(0);
-					};
-		
-					executeJob();
-				}
-		
-				main(...process.argv.slice(2)); */
-
-
-
-
-
-		/* 		new Promise(brokenLinks)
-					// parte 1
-					.then((value) => {
-						if (options.validate) {
-							const markdownLinkCheck = require('markdown-link-check');
-							links.forEach(link => {
-								markdownLinkCheck(link, (err, results) => {
-									if (err) {
-										console.error('Error', err)
-									}
-									results.forEach(result => {
-										new Promise(result, value)
-											.then((result, value) => {
-												if (stats === 0) {
-													console.log(route1, result.link, result.status, result.statusCode)
-												}
-												resolve(result, value);
-											})
-											.then((result, value) => {
-												if (result.status === "dead") {
-													value += 1
-													console.log("brokenLinks iteracion es: " + value)
-												}
-												resolve(result, value);
-											})
-		
-									})
-								})
-							})
-						}
-						resolve(value);
-					})
-					// parte 2
-					.then((value) => {
-						console.log("brokenLinks 1° .then es: " + brokenLinks)
-						return value
-					})
-					// parte 3
-					.then((value) => {
-						console.log("brokenLinks 2° .then es: " + brokenLinks)
-						if (options.stats) {
-							let duplicatesTotal = links.filter((item, index) => links.indexOf(item) !== index).length
-							console.log("Total: " + links.length)
-							console.log("Unique: " + (links.length - duplicatesTotal))
-							if (validate === 1) {
-								console.log("Broken: " + value)
-							}
-						}
-					}) */
-
-
-
-
-
 
 program.parse(process.argv)
 
@@ -294,15 +92,15 @@ const flags = {
 	},
 };
 
-const commands = {
+/* const commands = {
 	help: { desc: `Print help info` }
-};
+}; */
 
-const helpText = meowHelp({
+/* const helpText = meowHelp({
 	name: `md-links-alexv`,
 	flags,
 	commands
-});
+}); */
 
 /* const options = {
 	inferType: true,

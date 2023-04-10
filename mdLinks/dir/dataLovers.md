317ccd1399d0b7040f3a4ebdde204cc6ad4fd4dd
AlexVYard <alexvegayard@gmail.com>
2023-04-10 16:59:08 -0500

    ruta como directorio funcional

---
+++ mdLinks/dir/dataLovers.md
@@ -0,0 +1,100 @@
+# Data Lovers
+
+## Índice
+
+* [1. Resumen del proyecto](#2-resumen-del-proyecto)
+* [2. Herramientas usadas](#3-herramientas-usadas)
+* [3. Trabajo pre-codeo](#4-trabajo-pre-codeo)
+* [4. Funcionalidad de la Interfaz](#5-funcionalidad-de-la-Interfaz)
+* [5. Recursos](#6-recursos)
+
+***
+
+## 1. Resumen del proyecto
+
+Esta web nos permite visualizar una base de datos con los pokemons de las primeras 2 generaciones con su informacion correspondiente a Pokemon GO.
+Un trabajo de Alexandra F. Vega y Barbara de Villa.
+
+## 2. Herramientas usadas
+
+### HTML
+
+### CSS
+
+### JavaScript
+
+### Control de Versiones (Git y GitHub)
+
+## 3. Trabajo pre-codeo
+
+### Definición del producto
+
+Una web para visualizar datos y facilitar el uso de la aplicacion de Pokemon Go.
+
+### Historias de usuario
+
+![Historias de usuario](/src/img/historiasdeusuario.jpg)
+![Criterios de Aceptación](/src/img/criteriosdeaceptacion.jpg)
+
+### Diseño de la Interfaz de Usuario
+
+#### Prototipo de baja fidelidad
+
+![Prototipo de baja 1](/src/img/prototipodebaja1.jpg)
+
+#### Prototipo de alta fidelidad
+
+![Prototipo de alta 1](/src/img/prototipodealta1.jpg)
+![Prototipo de alta 2](/src/img/MacBookAir-2.png)
+![Prototipo de alta 3](/src/img/MacBookAir-3.png)
+
+#### Testeos de usabilidad
+
+No encontramos grandes problemas durante los testeos, pero gracias al feedback decidimos modificar la interfaz al diseño actual para tener una experiencia de usuario mas placentera y ordenada
+
+## 4. Funcionalidad de la Interfaz
+
+**1. Caracteristicas principales**
+
+* La interfaz esta dividida en 2 secciones importantes, en una podemos visualizar la informacion con tarjetas y la otra en forma de tabla.
+* En la seccion de tarjetas podemos ver la imagen numero nombre, y tipos de cada pokemon.
+* En la seccion con la tabla tenemos los mismos datos pero tambien incluyen debilidades, resistencias, Km para incubar huevos y el numero de caramelos necesarios para pasar a la siguiente evolucion.
+* Contamos con botones de filtrado por tipo, estos botones se pueden combinar para buscar pokemon que pertenezcan a 2 tipos distintos.
+* Tambien hay una caja de texto para ingresar el numero o el nombre de un pokemon y realizar su busqueda.
+* Al buscar un nombre podemos visualizar todos los pokemon de la linea evolutiva del pokemon buscado.
+* El header para los nombres y numeros es usado como boton para ordenar las columanas en ascendente o descendente.
+
+**2. Descripción de scripts / archivos**
+
+* `src/index.html`: Punto de entrada a la aplicación.
+* `src/style.css`: Reglas de estilo
+* `src/main.js`: Codigo para interactuar el el DOM y funcionalidad basica de la interfaz.
+* `src/data.js`: Codigo con la funcion para ordenar columnas y la funcion de filtrado por tipo.
+* `src/data/pokemon/pokemon.js`: Base de datos usada en el proyecto.
+* `test/data.spec.js`: Tests que se uso para verficar el codigo.
+* `README.md`: Explicacion del producto.
+* `src/img`: Imagenes usadas en la interfaz y el readme.
+
+## 5. Recursos
+
+### Software Usado
+
+* [Code](https://code.visualstudio.com/)
+* [UNIX Shell](https://curriculum.laboratoria.la/es/topics/shell)
+* [Git bash](https://git-scm.com/download/win)
+* [Git](https://git-scm.com/)
+* [GitHub](https://github.com/)
+* [GitHub Pages](https://pages.github.com/)
+
+### Referencias
+
+* [Generar una tabla con una base de datos](https://www.youtube.com/watch?v=ri5Nqe_IK50).
+* [Ordenar columnas](https://www.youtube.com/watch?v=0d76_2sksWY)
+* [Usar funciones en loops](https://stackoverflow.com/questions/18312016/calling-function-in-a-loop-in-javascript)
+* [Referencia para la tabla](https://pokemondb.net/pokedex/all)
+* [Saltar un loop](https://www.w3schools.com/js/js_break.asp)
+* [Busqueda](https://www.w3schools.com/jsref/jsref_includes_array.asp)
+* [Filtrado](https://www.youtube.com/watch?v=O3Ht2uejzfM)
+*[Tarjetas](https://www.youtube.com/watch?v=rfsv0i4qXFs)
+*[Diseño](https://www.youtube.com/watch?v=bF5UBkonkIs )
+
--- mdLinks/utils/cli.js
+++ mdLinks/utils/cli.js
@@ -10,61 +10,287 @@ program
 	.argument('<route1>', 'connect to the specified server')
 	.option('-v, --validate', 'Validate links, use with a route')
 	.option('-s, --stats', 'Show stats, use with a route')
-	.action((route1, options) => {
+	.action((route, options) => {
 
-		const { readFileSync } = require('fs');
-		const markdownLinkExtractor = require('markdown-link-extractor');
+		console.log("") // linea extra para que se vea bonito
 
-		const markdown = readFileSync(route1, { encoding: 'utf8' });	// string del archivo markdown
-		const { links } = markdownLinkExtractor(markdown);	// objeto con todos los links
+		import('linkinator')
+			.then(async module => {
 
-		const validate = options.validate ? 1 : 0;
-		const stats = options.stats ? 1 : 0;
+				const { readdirSync, readFileSync } = require('fs');
+				const markdownLinkExtractor = require('markdown-link-extractor');
+				const markdownLinkCheck = require('markdown-link-check');
 
-		let brokenLinks = 0
+				// const markdown = readFileSync(route, { encoding: 'utf8' });	// string del archivo markdown
+				const markdown2 = readdirSync(route, { encoding: 'utf8' })
+				// console.log(markdown2)
 
-		const brokenPromise = new Promise((resolve) => {
-				import('linkinator')
-					.then(module => {
-						const checker = new module.LinkChecker()
-						checker.on("link", (link) => {
-							if (link.state === "BROKEN") {
-								brokenLinks++
-							}
+				const checker = new module.LinkChecker()
+
+				const validate = options.validate ? 1 : 0;
+				const stats = options.stats ? 1 : 0;
+
+				for (i in markdown2) {
+					const markdown = readFileSync(`${route}/${markdown2[i]}`, { encoding: 'utf8' })
+					let { links } = markdownLinkExtractor(markdown)
+					let links2 = links.filter((item) => item.match(/^(https)+\:\/\//))
+					// console.log(links2)	
+
+					if ((validate === 0) && (stats === 1)) {
+						let duplicatesTotal = links2.filter((item, index) => links2.indexOf(item) !== index).length
+						console.log(markdown2[i])
+						console.log("  Total: " + links2.length)
+						console.log("  Unique: " + (links2.length - duplicatesTotal))
+						console.log("")
+					}
+
+					/* if ((validate === 1) && (stats === 0)) {
+
+						// const validatePromise = new Promise((resolve) => {
+							checker.on("link", (link) => {
+								console.log(markdown2[i], link.url, link.state, link.status)
+								if (link.state === "BROKEN") {
+									brokenLinks++
+								}
+							})
+							await checker.check({ path: `${route}/${markdown2[i]}` })
+							// console.log(markdown2[i], result.links[0].url, result.links[0].state, result.links[0].status);
+						// })
+
+						// validatePromise.then(() => {
+
+						// })
+						
+					} */
+
+					for (j in links2) {
+
+						if ((validate === 0) && (stats === 0)) console.log(markdown2[i], links2[j])
+
+						if ((validate === 1) && (stats === 0)) {
+							const result = await checker.check({ path: links2[j] });
+							console.log(markdown2[i], result.links[0].url, result.links[0].state, result.links[0].status);
+						}
+
+						/* if ((validate === 1) && (stats === 1)) {
+
+							let brokenLinks = 0
+	
+							const brokenPromise = new Promise((resolve) => {
+								const checker = new module.LinkChecker()
+								checker.on("link", (link) => {
+									console.log(link.state)
+									if (link.state === "BROKEN") {
+										brokenLinks++
+									}
+								})
+								resolve(checker.check({ path: links2 }))
+							})
+	
+							brokenPromise.then(() => {
+								if (validate === 1) {
+									console.log("Broken: " + brokenLinks)
+								}
+							})
+
+						} */
+
+					}
+
+					if ((validate === 1) && (stats === 1)) {
+
+						let brokenLinks = 0
+						
+						for (j in links2) {
+							const result = await checker.check({ path: links2[j] });
+							if (result.links[0].state === "BROKEN") brokenLinks++
+							// console.log(markdown2[i], result.links[0].url, result.links[0].state, result.links[0].status);
+						}
+
+						console.log(markdown2[i])
+						let duplicatesTotal = links2.filter((item, index) => links2.indexOf(item) !== index).length
+						console.log("  Total: " + links2.length)
+						console.log("  Unique: " + (links2.length - duplicatesTotal))
+						console.log("  Broken: " + brokenLinks)
+
+					}
+
+						// const brokenPromise = new Promise((resolve) => {
+						// 	// if ((validate === 1) && (stats === 1)) {
+						// 	checker.on("link", (link) => {
+						// 		// console.log(link.url, link.state)
+						// 		if (link.state === "BROKEN") {
+						// 			brokenLinks++
+						// 		}
+						// 	})
+						// 	resolve(checker.check({ path: `${route}/${markdown2[i]}` }))
+						// 	/* const result = checker.check({ path: links2[j] });
+						// 	if (result.links[0].state === 'BROKEN') {
+						// 		brokenLinks++
+						// 	} */
+						// 	// }
+						// })
+
+						/* brokenPromise.then(() => {
+							let duplicatesTotal = links2.filter((item, index) => links2.indexOf(item) !== index).length
+							console.log(markdown2[i])
+							console.log("  Total: " + links2.length)
+							console.log("  Unique: " + (links2.length - duplicatesTotal))
+							console.log("  Broken: " + brokenLinks)
+						}) */
+
+					}
+
+					/* if ((validate === 1) && (stats === 1)) {
+						brokenPromise.then(() => {
+							let duplicatesTotal = links2.filter((item, index) => links2.indexOf(item) !== index).length
+							console.log(markdown2[i])
+							console.log("  Total: " + links2.length)
+							console.log("  Unique: " + (links2.length - duplicatesTotal))
+							console.log("  Broken: " + brokenLinks)
 						})
-						resolve(checker.check({ path: route1 }))
-					});
+					} */
+
+					
+
+			})
+
+				/* checker.on("link", (link) => {
+					console.log(markdown2[i], links[j], link.state)
+					// process.stdout.write(link.state)
+					if (link.state === "BROKEN") {
+						brokenLinks++
+					}
+				}) */
+
+				/* await checker.check({ path: links2[j] }); */
 		})
 
-		if (validate === 1) {
-			const markdownLinkCheck = require('markdown-link-check');
-			links.forEach(link => {
-				markdownLinkCheck(link, (err, results) => {
-					if (err) {
-						console.error('Error', err)
-						return
-					}
-					results.forEach(result => {
-						if (stats === 0) {
-							console.log(route1, result.link, result.status, result.statusCode)
-						}
-					})
-				})
-			})
-		}
 
-		if (stats === 1) {
-			let duplicatesTotal = links.filter((item, index) => links.indexOf(item) !== index).length
-			console.log("Total: " + links.length)
-			console.log("Unique: " + (links.length - duplicatesTotal))
-			brokenPromise.then(() => {
-				if (validate === 1) {
-					console.log("Broken: " + brokenLinks)
+/* console.log(markdown2[i], links[j])
+// const brokenPromise = new Promise((resolve) => {
+checker.on('pagestart', url => {
+	console.log(`Scanning ${url}`);
+});
+	
+checker.on('link', result => {
+	console.log(`  ${result.url}`);
+}); */
+
+/* const result = await checker.check({
+	path: links[j],
+	// port: 8673,
+	// recurse: true,
+	// linksToSkip: [
+	//   'https://jbeckwith.com/some/link',
+	//   'http://example.com'
+	// ]
+}); */
+
+// console.log(result.passed ? 'PASSED :D' : 'FAILED :(');
+
+// console.log(markdown2[i], links[j])
+/* checker.check({
+	path: links[j]
+	// linksToSkip: [links[j]]
+});
+console.log(markdown2[i], links[j]) */
+// console.log(results["links"][0]["url"]);
+// }
+
+
+// const check = new module.check		
+/* checker.on("link", (link) => {
+	console.log(markdown2[i], links[j], link.state)
+	// process.stdout.write(link.state)
+	if (link.state === "BROKEN") {
+		brokenLinks++
+	}
+}) */
+
+// resolve(checker.check({ path: links[i] }))
+/*	checker.check({
+	path: links[j],
+	recurse: false,
+	linksToSkip: ["/src/img/historiasdeusuario.jpg"]
+}) */
+
+// })
+
+
+
+
+/* links.forEach(link => {
+	markdownLinkCheck(link, (err, results) => {
+		if (err) {
+			console.error('Error', err)
+			return
+		}
+		results.forEach(result => {
+			if ((validate === 0) && (stats === 0)) console.log(markdown2[i], result.link)
+			if ((validate === 1) && (stats === 0)) console.log(markdown2[i], result.link, result.status, result.statusCode)
+		})
+	})
+}) */
+
+/* if (stats === 1) {
+	let duplicatesTotal = links.filter((item, index) => links.indexOf(item) !== index).length
+	console.log("Total: " + links.length)
+	console.log("Unique: " + (links.length - duplicatesTotal))
+	brokenPromise.then(() => {
+		if (validate === 1) {
+			console.log("Broken: " + brokenLinks)
+		}
+	})
+} */
+
+// const { links } = markdownLinkExtractor(markdown);	// objeto con todos los links		
+// console.log(readFileSync(links[10], { encoding: 'utf8' }))
+// console.log(links)
+
+/* const validate = options.validate ? 1 : 0;
+const stats = options.stats ? 1 : 0;
+
+let brokenLinks = 0
+
+const brokenPromise = new Promise((resolve) => {
+	import('linkinator')
+		.then(module => {
+			const checker = new module.LinkChecker()
+			checker.on("link", (link) => {
+				if (link.state === "BROKEN") {
+					brokenLinks++
 				}
 			})
-		}
+			resolve(checker.check({ path: route1 }))
+		});
+})
 
+const markdownLinkCheck = require('markdown-link-check');
+	
+links.forEach(link => {
+	markdownLinkCheck(link, (err, results) => {
+		if (err) {
+			console.error('Error', err)
+			return
+		}
+		results.forEach(result => {
+			if ((validate === 0) && (stats === 0)) console.log(route1, result.link)
+			if ((validate === 1) && (stats === 0)) console.log(route1, result.link, result.status, result.statusCode)
+		})
 	})
+})
+
+if (stats === 1) {
+	let duplicatesTotal = links.filter((item, index) => links.indexOf(item) !== index).length
+	console.log("Total: " + links.length)
+	console.log("Unique: " + (links.length - duplicatesTotal))
+	brokenPromise.then(() => {
+		if (validate === 1) {
+			console.log("Broken: " + brokenLinks)
+		}
+	})
+} */
 
 program.parse(process.argv)
 
